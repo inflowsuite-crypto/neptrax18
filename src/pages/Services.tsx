6668dc967f25e44b799842af588569612e6e39f4
inflowsuite-crypto <inflowsuite@gmail.com>
2025-11-20 08:31:34 -0800

    Updated Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -307,7 +307,7 @@ export default function Services({ onNavigate }: ServicesProps) {
 </span><br />
                 <span className="text-line" style={{ animationDelay: '0.3s' }}>Reclaim your time for</span><br />
                 <span className="text-line" style={{ animationDelay: '0.4s' }}>What truly grows your</span><br />
-                <span className="text-line" style={{ animationDelay: '0.5s' }}>business online from your</span><br />
+                <span className="text-line" style={{ animationDelay: '0.5s' }}>business online from an</span><br />
                 <span className="text-line" style={{ animationDelay: '0.6s' }}>Idea to a fully custom Website,</span><br />
               </h2>
 
